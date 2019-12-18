1e4307b36b7f7ebce67b7131a3e36c937e455e20
csslick <digitone@naver.com>
2019-12-18 17:14:50 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// main.js
+$(function(){
+
+  $('.bx-slider').bxSlider({
+    // 여기에 옵션 설정
+    mode: 'horizontal',     // 효과설정
+    auto: false,             // 자동재생
+    easing: "ease-in-out",  // 가속도(timing-function)
+    pause: 4000,            // 정지시간(대기)
+    controls: false,        // prev, next 표시/비표시
+    pager: true,           // 하단 인디케이터 표시/비표시
+  });
+
+});
