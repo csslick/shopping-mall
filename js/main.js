eeac0bac3627ac2731c1de4d3ce69db384deb15f
csslick <digitone@naver.com>
2019-12-19 11:54:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 // main.js
 $(function(){
 
+  // 상품리스트 추가
+  var item_list = $('.item_list');
+  var list = '';
+  var list_num = 5;
+  var kwd = '원';
+  var origin_price = '';
+
+  for(var i = 0; i < list_num; i++) {
+    list += '<li>';
+    list += '<a href="#">';
+    list += '<img src="' + product_data[i].img_url + '" alt="노카라 트위드 자켓">';
+    list += '<span class="title">' + product_data[i].title + '</span>';
+    list += '<span class="name">' + product_data[i].name + '</span>'
+
+    if(product_data[i].origin_price == "") {
+      kwd = "&nbsp;";
+      origin_price = "";
+    } else { origin_price = product_data[i].origin_price;}
+
+    list += '<del class="origin_price">' + origin_price + kwd + '</del>';
+    list += '<ins class="sale_price"><strong>' + product_data[i].sale_price + '원</strong></ins>';
+  }
+
+  item_list.append(list);
+  
+  
+  
   $('.bx-slider').bxSlider({
     // 여기에 옵션 설정
     mode: 'horizontal',     // 효과설정
