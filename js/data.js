22171f63d3b65639fefde60c21e8f0ef19fee43d
csslick <digitone@naver.com>
2019-12-19 12:51:52 +0900

    Add files via upload
    카테고리 메뉴, 메인컬러 UI, 상품데이터 기능 추가

---
+++ js/data.js
@@ -0,0 +1,54 @@
+// data.js
+
+/** 제품 정보 데이터(JSON) */
+var product_data = [
+  {
+    // 속성: 값
+    img_url: "images/item001.jpg",
+    title: "EGOIST",
+    name: "날개사 볼륨소매 풀오버 EK4KL065",
+    origin_price: "268,000",
+    sale_price: "178,800"
+  },
+  {
+    // 속성: 값
+    img_url: "http://placehold.it/225x323&text=2",
+    title: "제품 브랜드명2",
+    name: "제품명2",
+    origin_price: "368,000",
+    sale_price: "214,800"
+  },
+  {
+    // 속성: 값
+    img_url: "http://placehold.it/225x323&text=3",
+    title: "제품 브랜드명3",
+    name: "제품명3",
+    origin_price: "368,000",
+    sale_price: "214,800"
+  },
+  {
+    // 속성: 값
+    img_url: "http://placehold.it/225x323&text=4",
+    title: "제품 브랜드명4",
+    name: "제품명4",
+    origin_price: "368,000",
+    sale_price: "214,800"
+  },
+  {
+    // 속성: 값
+    img_url: "http://placehold.it/225x323&text=5",
+    title: "제품 브랜드명5",
+    name: "제품명5",
+    origin_price: "368,000",
+    sale_price: "214,800"
+  },
+  {
+    // 속성: 값
+    img_url: "http://placehold.it/225x323&text=6",
+    title: "제품 브랜드명6",
+    name: "제품명6",
+    origin_price: "368,000",
+    sale_price: "214,800"
+  },
+];
+
--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@
 $(function(){
 
   // 상품리스트 추가
-  var item_list = $('.item_list');
-  var list = '';
-  var list_num = 5;
+  var item_list = $('.item_list'); // 상품 목록 위치
+  var list = '';  // 삽입될 html data
+  var list_num = 5;   // 상품목록 개수
   var kwd = '원';
   var origin_price = '';
 
   for(var i = 0; i < list_num; i++) {
     list += '<li>';
     list += '<a href="#">';
-    list += '<img src="' + product_data[i].img_url + '" alt="노카라 트위드 자켓">';
+    list += '<img src="' + product_data[i].img_url + '">';
     list += '<span class="title">' + product_data[i].title + '</span>';
     list += '<span class="name">' + product_data[i].name + '</span>'
 
@@ -26,12 +26,17 @@ $(function(){
 
   item_list.append(list);
   
-  
-  
+
+  // 카테고리 토글 버튼
+  $('#category_btn').click(function(){
+    $('#category').slideToggle();
+  });
+
+  // BX슬라이더
   $('.bx-slider').bxSlider({
     // 여기에 옵션 설정
     mode: 'horizontal',     // 효과설정
-    auto: false,             // 자동재생
+    auto: true,             // 자동재생
     easing: "ease-in-out",  // 가속도(timing-function)
     pause: 4000,            // 정지시간(대기)
     controls: false,        // prev, next 표시/비표시
