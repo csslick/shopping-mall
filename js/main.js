2b3fcfb006b6ff9e1ff6a79bc19b882434a7690e
csslick <digitone@naver.com>
2019-12-22 20:33:07 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // main.js
 
-// 상품목록 추가(all | man | woman, 목록개수)
-function show_product(category_name, list_num) {
-  var item_list = $('.item_list'); // 상품 목록 위치
+// 상품목록 추가(요소명, all | man | woman, 목록개수)
+function show_product(el, category_name, list_num) {
+  var item_list = $(el); // 상품 목록 위치
   var list = '';  // 삽입될 html data
   var kwd = '원';
   var origin_price = '';
@@ -44,8 +44,10 @@ function show_product(category_name, list_num) {
 
 $(function(){
 
-  // 상품목록 추가(all | man | woman, 목록개수)
-  show_product('all', 10);
+  // 상품목록 추가(요소명, all | man | woman, 목록개수)
+  show_product('#top_seller ul', 'all', 10);
+  show_product('#new_product ul', 'all', 5);
+
   $('.category_menu > a').eq(0).css('text-decoration', 'underline');
 
   // 카테고리 토글 버튼
@@ -66,7 +68,7 @@ $(function(){
     var idx = $(this).index();
     var category_name = $(this).attr('class');
     console.log(category_name)
-    show_product(category_name, 10);
+    show_product('#top_seller ul', category_name, 10);
     return false;
   });
 
